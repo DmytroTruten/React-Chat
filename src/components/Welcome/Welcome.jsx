e5c7205a3fc3ac8eaf2b40550b5cd81394c26a1c
DmitriyTruten <dmitriy2503a@gmail.com>
2023-03-17 18:58:34 +0200

    add firebase dependecies

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,8 +1,21 @@
+import React, { useState } from "react";
+import { auth } from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 import logo from "../../logo.svg";
-import signInImg from "../../assets/google-sign-in.png"
+import signInImg from "../../assets/google-sign-in.png";
 import "./Welcome.css";
 
 function App() {
+  const [user, setUser] = useState(false);
+
+  const googleSignIn = () => {
+    setUser(true);
+  };
+
+  const signOut = () => {
+    setUser(false);
+  };
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
@@ -10,7 +23,7 @@ function App() {
       <p className="Sign-text">
         Sign in with Google to chat with your friends.
       </p>
-      <button className="Sign-button">
+      <button className="Sign-button" onClick={googleSignIn}>
         <img src={signInImg} alt="" />
       </button>
     </div>
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuqTPTJpE2ChBfow3m7Bvq31AJANSD3Uw",
+  authDomain: "react-chat-tdv.firebaseapp.com",
+  projectId: "react-chat-tdv",
+  storageBucket: "react-chat-tdv.appspot.com",
+  messagingSenderId: "700892868808",
+  appId: "1:700892868808:web:d76fc8ec4965aa6ac9b5fd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export const db = getFirestore(app);

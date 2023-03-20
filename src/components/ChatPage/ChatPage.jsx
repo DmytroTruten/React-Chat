e36fdb81587e86590b43d50b9810d5d8d073aa5d
DmitriyTruten <dmitriy2503a@gmail.com>
2023-03-20 17:25:13 +0200

    adjust ChatPage component

--- src/components/ChatPage/ChatPage.jsx
+++ src/components/ChatPage/ChatPage.jsx
@@ -1,13 +1,43 @@
 import React from "react";
-import SendMessage from "../SendMessageForm/SendMessageForm";
+import SendMessageForm from "../SendMessageForm/SendMessageForm";
+import { useEffect, useState } from "react";
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
+} from "firebase/firestore";
+import { db } from "../../firebase";
 import "./ChatPage.css";
+import Message from "../Message/Message";
 
-function ChatPage () {
-  return(
+function ChatPage() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "messages"),
+      orderBy("createdAt"),
+      limit(50)
+    );
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let messages = [];
+      QuerySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => unsubscribe;
+  }, []);
+  return (
     <div className="ChatPage">
-        <SendMessage />
+      {messages?.map((message) => {
+        <Message key={message.id} message={message} />;
+      })}
+      <SendMessageForm />
     </div>
-  )
+  );
 }
 
 export default ChatPage;

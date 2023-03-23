a12ef133d1c8358c288fddb5942fb0b2dec4c2a0
DmitriyTruten <dmitriy2503a@gmail.com>
2023-03-23 11:44:01 +0200

    add timestamp to message component

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { auth } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import "./Message.css"
+import moment from "moment";
+import "./Message.css";
 
-function Message({message}) {
+function Message({ message }) {
   const [user] = useAuthState(auth);
 
   return (
-    <div className={`ChatBubble ${message.uid === user.uid ? "right" : "left"}`}>
-      <img
-        className="UserAvatar"
-        src={message.avatar}
-        alt="UserAvatar"
-      />
+    <div
+      className={`ChatBubble ${message.uid === user.uid ? "right" : "left"}`}
+    >
+      <img className="UserAvatar" src={message.avatar} alt="UserAvatar" />
       <div className="chat-bubble__right">
         <p className="user-name">{message.name}</p>
         <p className="user-message">{message.text}</p>
+        <p className="user-message">
+          {message.createdAt !== null
+            ? moment.unix(message.createdAt.seconds).format("hh:mm")
+            : null}
+        </p>
       </div>
     </div>
   );

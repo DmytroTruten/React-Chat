856d0e280630bcaee3095141f232c53e73b3fae1
DmitriyTruten <dmitriy2503a@gmail.com>
2023-03-26 20:24:59 +0300

    reinitialize whole project with vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { auth } from "./firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
-import NavBar from "./components/NavBar/NavBar";
-import Welcome from "./components/Welcome/Welcome";
-import ChatPage from "./components/ChatPage/ChatPage";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [user] = useAuthState(auth);
 
   return (
     <div className="App">
-      <NavBar />
-      {!user ? <Welcome /> : <ChatPage />}
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>    
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

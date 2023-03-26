7de445fc2a4277be5a8074218d6273fbe91e9d4b
DmitriyTruten <dmitriy2503a@gmail.com>
2023-03-26 23:23:36 +0300

    add LogIn/SignUp components, basic styles

---
+++ src/pages/LogIn.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button';
+import "../styles/LogIn/LogIn.css";
+
+const LogIn = () => {
+  return (
+    <div className="LogInContainer row justify-content-center align-items-center h-100 mx-0 my-0">
+      <Form className="LogInForm col-4 d-flex flex-column align-items-center py-5">
+        <h1 className="LogInHeader">React Chat</h1>
+        <h5 className="LogInSubheader">Log In</h5>
+        <Form.Group>
+          <Form.Control
+            className="LogInControl my-3"
+            type="text"
+            placeholder="Login"
+          ></Form.Control>
+          <Form.Control
+            className="LogInPasswordControl my-3"
+            type="password"
+            placeholder="Password"
+          ></Form.Control>
+        </Form.Group>
+        <Button className="mt-3" type="submit">Sign in</Button>
+      </Form>
+    </div>
+  );
+};
+
+export default LogIn;

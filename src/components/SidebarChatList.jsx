54f28d6da2226d3f65410535b9eac8413bc36824
DmitriyTruten <dmitriy2503a@gmail.com>
2023-04-05 19:05:27 +0300

    add fetching chats conversations

--- src/components/SidebarChatList.jsx
+++ src/components/SidebarChatList.jsx
@@ -1,130 +1,52 @@
-import React from "react";
-import logo from "../assets/react.svg";
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { db } from "../firebase";
 import "../styles/SidebarChatList/SidebarChatList.css";
 
 const SidebarChatList = () => {
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(
+        doc(db, "usersChats", currentUser.uid),
+        (doc) => {
+          setChats(doc.data());
+        }
+      );
+      return () => {
+        unsub();
+      };
+    };
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+  console.log(Object.entries(chats));
   return (
     <div className="SidebarChatList">
-      <div className="SidebarChat d-flex">
-        <div className="SidebarChatImgContainer d-flex justify-content-center align-items-center">
-          <img src={logo} alt="" />
-        </div>
-        <div className="SidebarChatInfo d-flex flex-column w-100">
-          <div className="SidebarChatUsernameContainer d-flex justify-content-between">
-            <p className="SidebarChatUsername">Username Example</p>
-            <p className="SidebarChatTime">12:34</p>
+      {Object.entries(chats)?.map((chat) => (
+        <div className="SidebarChat d-flex" key={chat[0]}>
+          <div className="SidebarChatImgContainer d-flex justify-content-center align-items-center">
+            <img
+              className="UserAvatar"
+              src={chat[1].userInfo.photoURL}
+              alt=""
+            />
           </div>
-          <p className="SidebarChatLastMsg">Last Message Example</p>
-        </div>
-      </div>
-      <div className="SidebarChat d-flex">
-        <div className="SidebarChatImgContainer d-flex justify-content-center align-items-center">
-          <img src={logo} alt="" />
-        </div>
-        <div className="SidebarChatInfo d-flex flex-column w-100">
-          <div className="SidebarChatUsernameContainer d-flex justify-content-between">
-            <p className="SidebarChatUsername">Username Example</p>
-            <p className="SidebarChatTime">12:34</p>
+          <div className="SidebarChatInfo d-flex flex-column w-100">
+            <div className="SidebarChatUsernameContainer d-flex justify-content-between">
+              <p className="SidebarChatUsername">
+                {chat[1].userInfo.displayName}
+              </p>
+              <p className="SidebarChatTime">12:34</p>
+            </div>
+            <p className="SidebarChatLastMsg">
+              {chat[1].userInfo.lastMessage?.text}
+            </p>
           </div>
-          <p className="SidebarChatLastMsg">Last Message Example</p>
         </div>
-      </div>
-      <div className="SidebarChat d-flex">
-        <div className="SidebarChatImgContainer d-flex justify-content-center align-items-center">
-          <img src={logo} alt="" />
-        </div>
-        <div className="SidebarChatInfo d-flex flex-column w-100">
-          <div className="SidebarChatUsernameContainer d-flex justify-content-between">
-            <p className="SidebarChatUsername">Username Example</p>
-            <p className="SidebarChatTime">12:34</p>
-          </div>
-          <p className="SidebarChatLastMsg">Last Message Example</p>
-        </div>
-      </div>
-      <div className="SidebarChat d-flex">
-        <div className="SidebarChatImgContainer d-flex justify-content-center align-items-center">
-          <img src={logo} alt="" />
-        </div>
-        <div className="SidebarChatInfo d-flex flex-column w-100">
-          <div className="SidebarChatUsernameContainer d-flex justify-content-between">
-            <p className="SidebarChatUsername">Username Example</p>
-            <p className="SidebarChatTime">12:34</p>
-          </div>
-          <p className="SidebarChatLastMsg">Last Message Example</p>
-        </div>
-      </div>
-      <div className="SidebarChat d-flex">
-        <div className="SidebarChatImgContainer d-flex justify-content-center align-items-center">
-          <img src={logo} alt="" />
-        </div>
-        <div className="SidebarChatInfo d-flex flex-column w-100">
-          <div className="SidebarChatUsernameContainer d-flex justify-content-between">
-            <p className="SidebarChatUsername">Username Example</p>
-            <p className="SidebarChatTime">12:34</p>
-          </div>
-          <p className="SidebarChatLastMsg">Last Message Example</p>
-        </div>
-      </div>
-      <div className="SidebarChat d-flex">
-        <div className="SidebarChatImgContainer d-flex justify-content-center align-items-center">
-          <img src={logo} alt="" />
-        </div>
-        <div className="SidebarChatInfo d-flex flex-column w-100">
-          <div className="SidebarChatUsernameContainer d-flex justify-content-between">
-            <p className="SidebarChatUsername">Username Example</p>
-            <p className="SidebarChatTime">12:34</p>
-          </div>
-          <p className="SidebarChatLastMsg">Last Message Example</p>
-        </div>
-      </div>
-      <div className="SidebarChat d-flex">
-        <div className="SidebarChatImgContainer d-flex justify-content-center align-items-center">
-          <img src={logo} alt="" />
-        </div>
-        <div className="SidebarChatInfo d-flex flex-column w-100">
-          <div className="SidebarChatUsernameContainer d-flex justify-content-between">
-            <p className="SidebarChatUsername">Username Example</p>
-            <p className="SidebarChatTime">12:34</p>
-          </div>
-          <p className="SidebarChatLastMsg">Last Message Example</p>
-        </div>
-      </div>
-      <div className="SidebarChat d-flex">
-        <div className="SidebarChatImgContainer d-flex justify-content-center align-items-center">
-          <img src={logo} alt="" />
-        </div>
-        <div className="SidebarChatInfo d-flex flex-column w-100">
-          <div className="SidebarChatUsernameContainer d-flex justify-content-between">
-            <p className="SidebarChatUsername">Username Example</p>
-            <p className="SidebarChatTime">12:34</p>
-          </div>
-          <p className="SidebarChatLastMsg">Last Message Example</p>
-        </div>
-      </div>
-      <div className="SidebarChat d-flex">
-        <div className="SidebarChatImgContainer d-flex justify-content-center align-items-center">
-          <img src={logo} alt="" />
-        </div>
-        <div className="SidebarChatInfo d-flex flex-column w-100">
-          <div className="SidebarChatUsernameContainer d-flex justify-content-between">
-            <p className="SidebarChatUsername">Username Example</p>
-            <p className="SidebarChatTime">12:34</p>
-          </div>
-          <p className="SidebarChatLastMsg">Last Message Example</p>
-        </div>
-      </div>
-      <div className="SidebarChat d-flex">
-        <div className="SidebarChatImgContainer d-flex justify-content-center align-items-center">
-          <img src={logo} alt="" />
-        </div>
-        <div className="SidebarChatInfo d-flex flex-column w-100">
-          <div className="SidebarChatUsernameContainer d-flex justify-content-between">
-            <p className="SidebarChatUsername">Username Example</p>
-            <p className="SidebarChatTime">12:34</p>
-          </div>
-          <p className="SidebarChatLastMsg">Last Message Example</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };

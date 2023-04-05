10bdd4f852e067ff2f639ba6fc2e4adda2fb4105
DmitriyTruten <dmitriy2503a@gmail.com>
2023-04-05 20:50:44 +0300

    add fetching messages for certain chat

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 import "../styles/Message/Message.css";
 
-const Message = () => {
-  return <div className="MessageContainer d-flex">
-    <p className="Message">Message Example</p>
-    <p className="MessageTime d-flex">12:34</p>
-  </div>;
+const Message = ({ message }) => {
+  return (
+    <div className="MessageContainer d-flex">
+      <p className="Message">Message Example</p>
+      <p className="MessageTime d-flex">12:34</p>
+    </div>
+  );
 };
 
 export default Message;
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,30 +1,32 @@
-import React from 'react';
-import Message from './Message';
-import "../styles/Messages/Messages.css"
+import React, { useContext, useState } from "react";
+import Message from "./Message";
+import "../styles/Messages/Messages.css";
+import { useEffect } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { ChatContext } from "../context/ChatContext";
+import { db } from "../firebase";
 
 const Messages = () => {
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
+
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, "chats", data.chatID), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, [data.chatID]);
+
   return (
     <div className="Messages d-flex flex-column align-items-end">
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
+      {messages?.map((message) => (
+        <Message message={message} key={message.id} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default Messages;

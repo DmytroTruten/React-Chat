d3d18c3e00f99f09086dd1c466c5bd09fb0817df
DmitriyTruten <dmitriy2503a@gmail.com>
2023-04-14 17:01:45 +0300

    add styles for messages with imgs/caption

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -13,17 +13,24 @@ const Message = ({ message }) => {
     <Fragment>
       <div
         className={`${
-          message.image ? "ImageContainer flex-column" : "MessageContainer"
+          message.image
+            ? `ImageContainer ${
+                message.text ? "WithCaption" : "WithoutCaption"
+              } flex-column`
+            : "MessageContainer"
         } d-flex`}
       >
-        <p className="Message">{message.text}</p>
-        <p className="MessageTime d-flex">
-          {moment.unix(message.date.seconds).format("HH:mm")}
-        </p>
-        {console.log(message)}
         {message.image && (
           <img className="MessageImage" src={message.image} alt="" />
         )}
+        <div className="MessageTextContainer d-flex justify-content-between">
+          {message.text && <p className="Message">{message.text}</p>}
+
+          <p className="MessageTime d-flex">
+            {moment.unix(message.date.seconds).format("HH:mm")}
+          </p>
+        </div>
+        {console.log(message)}
       </div>
     </Fragment>
   );

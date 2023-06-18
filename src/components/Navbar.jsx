6b9d1eaed49ddab9f2a4e050321fca5a1a1ce817
Dmytro Truten <dmitriy2503a@gmail.com>
2023-06-18 21:48:39 +0300

    adjust classname for saved msg user avatar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import { useContext } from 'react';
-import { ChatContext } from '../context/ChatContext';
-import "../styles/Navbar/Navbar.css"
+import React from "react";
+import { useContext } from "react";
+import { ChatContext } from "../context/ChatContext";
+import "../styles/Navbar/Navbar.css";
 
 const Navbar = () => {
-  const {data} = useContext(ChatContext)
+  const { data } = useContext(ChatContext);
 
-  return(
-    <div className='Navbar d-flex'>
-      <img className="UserAvatar" src={data.user?.photoURL} alt="" />
-      <p className='NavbarUsername'>{data.user?.displayName}</p>
+  return (
+    <div className="Navbar d-flex">
+      <img
+        className={`UserAvatar ${
+          data.user?.displayName === "Saved Messages"
+            ? "SavedMessagesUserAvatar"
+            : ""
+        }`}
+        src={data.user?.photoURL}
+        alt=""
+      />
+      <p className="NavbarUsername">{data.user?.displayName}</p>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
